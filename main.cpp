bca540b4f0f5c82144fa2c437e508a097d8c932f
CHKIM <ccmajkim1@gmail.com>
2020-05-22 07:14:44 -0700

    CtoF
    Relearning the use of int in C++ and modified a Fahrenheit to Celsius conversion program.

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@ int main() {
 
     double tempc;
     double tempf;
+    
     //Diaglogue for user input
     std::cout << "Enter the temperature in Celcius:\n";
+    
     //Input from user
     std::cin >> tempc;
-
+    
     //Celcius to Fahrenheit formula C * 9/5 + 32
     tempf = (tempc * 9/5) + 32;
 }
